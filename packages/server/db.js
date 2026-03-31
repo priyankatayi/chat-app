e98a16b75933313ab660603b09193b595e38e6c9
Priyanka <tayi.priyanka@gmail.com>
2026-03-31 17:37:13 -0400

    db changes

--- packages/server/db.js
+++ packages/server/db.js
@@ -1,14 +1,12 @@
 //connect postgres to JS
-
 const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  database: process.env.DATABASE_NAME,
-  host: process.env.DATABASE_HOST,
-  password: process.env.DATABASE_PASSWORD,
-  user: process.env.DATABASE_USER,
-  port: process.env.DATABASE_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
