715378bab4fb6ac5186afa8ebe561181f1371d61
Priyanka <tayi.priyanka@gmail.com>
2026-03-31 17:23:30 -0400

    Redis changes

--- packages/server/redis.js
+++ packages/server/redis.js
@@ -1,13 +1,24 @@
 const { createClient } = require("redis");
 
 const redisClient = createClient({
-  socket: {
-    host: "127.0.0.1",
-    port: 6379,
-  },
+  url: process.env.REDIS_URL,
 });
-redisClient.on("error", (err) => console.error("Redis error:", err));
 
-redisClient.connect();
+redisClient.on("error", (err) => {
+  console.error("Redis error:", err);
+});
+
+async function connectRedis() {
+  try {
+    if (!redisClient.isOpen) {
+      await redisClient.connect();
+      console.log("Redis connected");
+    }
+  } catch (err) {
+    console.error("Redis connection failed:", err);
+  }
+}
+
+connectRedis();
 
 module.exports = redisClient;
